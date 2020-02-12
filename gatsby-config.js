0198a2ca42845b5af905ce4cea0908fc86947eb0
Marcos-An <loseonjr@gmail.com>
2020-02-12 11:44:12 -0300

    First

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,13 @@
 const config = require('./src/data/config');
 
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${process.env.NODE_ENV}`
 });
-
 module.exports = {
   siteMetadata: {
     title: config.defaultTitle,
     description: config.defaultDescription,
-    author: config.author,
+    author: config.author
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -22,24 +21,24 @@ module.exports = {
         fieldName: 'github',
         url: 'https://api.github.com/graphql',
         headers: {
-          Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`
         },
-        fetchOptions: {},
-      },
+        fetchOptions: {}
+      }
     },
     {
       resolve: 'gatsby-plugin-nprogress',
       options: {
         color: config.themeColor,
-        showSpinner: false,
-      },
+        showSpinner: false
+      }
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: config.googleAnalyticsID,
-        head: true,
-      },
+        head: true
+      }
     },
     {
       resolve: 'gatsby-plugin-favicon',
@@ -55,9 +54,9 @@ module.exports = {
           firefox: true,
           twitter: false,
           yandex: false,
-          windows: false,
-        },
-      },
+          windows: false
+        }
+      }
     },
     {
       resolve: 'gatsby-plugin-manifest',
@@ -68,9 +67,9 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'minimal-ui',
-        icon: './static/favicon/favicon-512.png',
-      },
+        icon: './static/favicon/favicon-512.png'
+      }
     },
-    'gatsby-plugin-offline',
-  ],
+    'gatsby-plugin-offline'
+  ]
 };
